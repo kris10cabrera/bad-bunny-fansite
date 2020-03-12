5992a9581fdc0f738754649d50224c8c20392701
kris10cabrera <cabrera.kristen@gmail.com>
2020-03-12 19:19:33 -0400

    testing out word

--- src/components/Word.js
+++ src/components/Word.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 class Word extends React.Component {
   render() {
-    const { title, index, data } = this.props;
+    const { title, index, data, songs, times } = this.props;
     return(
       <div className="word-folder">
-        {title}
+        <h2 className="cr">{title}</h2>
 
       </div>
     )
